33a8873cf84764a0b387656c46e8e44434677b9e
bliinmaker <kir05062005@gmail.com>
2024-06-10 01:26:35 +0300

    feat: routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,51 +1,76 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { Layout } from '../components/layout/Layout'
-import { Auth } from '../pages/Auth'
-import { PageNotFound } from '../pages/PageNotFound'
-import { Home } from '../pages/Home'
-import Profile from '../pages/Profile'
-import { Register } from '../pages/Register'
-import Timetable from '../pages/Timetable'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Layout } from "../components/layout/Layout";
+import { Auth } from "../pages/Auth";
+import { PageNotFound } from "../pages/PageNotFound";
+import { Teachers } from "../pages/Teachers";
+import { Profile } from "../pages/Profile";
+import { Register } from "../pages/Register";
+import { Timetable } from "../pages/Timetable";
+import { WaitingForAccess } from "../pages/WaitingForAccess";
+import { ProtectedRoute } from "../components/protectedRoute/ProtectedRoute";
 
 export const appRoutes = [
-    {
-        path: 'auth',
+  {
+    path: "auth",
+    element: <Auth />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "waitingForAccess",
+    element: <WaitingForAccess />,
+  },
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Outlet />
+      </Layout>
+    ),
+    children: [
+      {
+        path: "*",
+        element: (
+          <ProtectedRoute>
+            <PageNotFound />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/",
+        element: (
+          <ProtectedRoute>
+            <Timetable />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "auth",
         element: <Auth />,
-    },
-    {
-        path: 'register',
+      },
+      {
+        path: "register",
         element: <Register />,
-    },
-	{
-        
-		path: '/',
-		element: (
-			<Layout>
-				<Outlet />
-			</Layout>
-		),
-		children: [
-			{
-				path: '*',
-				element: <PageNotFound />,
-			},
-			{
-				path: '/',
-				element: <Timetable />
-			},
-			// {
-			// 	path: 'auth',
-			// 	element: <Auth />,
-			// },
-            // {
-			// 	path: 'register',
-			// 	element: <Register />,
-			// },
-            {
-				path: 'profile',
-				element: <Profile />,
-			},
-		],
-	},
-]
+      },
+      {
+        path: "profile",
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "teachers",
+        element: (
+          <ProtectedRoute>
+            <Teachers />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+];

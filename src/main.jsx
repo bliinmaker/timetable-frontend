579bbc5b033d0846201ff3e5dbb1f8d85a62f9f7
bliinmaker <kir05062005@gmail.com>
2024-06-10 01:27:05 +0300

    feat: createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom";
+import { App } from "./App";
+import "./index.css";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-)
+const rootElement = document.getElementById("root");
+
+if (!rootElement.hasChildNodes()) {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
